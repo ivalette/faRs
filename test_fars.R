9bcf237d10cce22a51281289a79570b0f41ae397
Isabelle Valette <ivalette@icloud.com>
2017-01-21 16:37:55 +0100

    bug hunting

---
+++ test_fars.R
@@ -0,0 +1,12 @@
+library(testthat)
+
+# context("Test 1: Verifying exceptions for the fars_read() function: ")
+# test_that("The function does not return expected file names", 
+# {expect_equal(fars_read("test"), throws_error()})
+
+context("Test 2: Verifying the naming convention for integer and lists: ")
+test_that("The function does not return expected file names", 
+{expect_equal(make_filename(2013), "accident_2013.csv.bz2"})
+
+test_that("The function does not return expected file names for lists", 
+{expect_equal(make_filename(list(2013)), "accident_2013.csv.bz2"})
